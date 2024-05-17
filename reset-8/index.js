185108d3ada35dbcfc1914189d3d608bf195f2e9
MD IMRUL MOLLA <imrulmolla99@gmail.com>
2024-05-17 15:31:40 +0600

    added the solve problem

---
+++ reset-8/index.js
@@ -0,0 +1,30 @@
+//Problem solving 1......
+
+var orange = 200;
+var apples = 150;
+var myTotalTaka = 1000;
+var totalFruits = orange + apples;
+
+var returnTaka = myTotalTaka - totalFruits;
+console.log(returnTaka);
+
+
+// Problem solving 2.....
+
+var Mathemmatics = 75.23;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+
+var averageMarks = ( Mathemmatics + Biology + Chemistry + Physics + Bangla ) / 5;
+var FloatAverageMarks = parseFloat(averageMarks.toFixed(2));
+console.log(FloatAverageMarks);
+
+
+// problem solving.......
+ var num = 119;
+ var num2 = 5;
+
+var remainder = num % num2;
+console.log(remainder)
