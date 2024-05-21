ee4933286806fdafd009abaedec58f2bb1e146bb
imrul099 <imrulmolla99@gmail.com>
2024-05-21 19:50:55 +0600

    solved a problem about bmi

---
+++ JS-conditionals-tasks/1 Free Drinks.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const burger = 500;
+// const burger = 700;
+if(burger < 600){
+    console.log('free cake')
+}else
+{
+    console.log(30 + "tk")
+}
